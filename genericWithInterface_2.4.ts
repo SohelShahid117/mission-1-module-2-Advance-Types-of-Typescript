548cbf0ef61c850466dd62557f20733324d15e06
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-05 16:34:37 +0600

    2-4: Generic with Interface done

--- genericWithInterface_2.4.ts
+++ genericWithInterface_2.4.ts
@@ -1,7 +1,42 @@
 {
 //2-4: Generic with Interface
 
+interface Developer <T,U=null> {
+    name:string;
+    computer:{
+        brand:string;
+        model:string;
+        releaseYear:number
+    };
+    // smartWatch:string
+    smartWatch:T; //age janina j type ki hobe.tai type dore nilam T.
+    bike?:U 
+}
 
+interface CasioWatch {
+    brand:string,
+    model:string,
+    price:number
+};
+// const poorDeveloper:Developer<string>={
+// const poorDeveloper:Developer<object>={
+// const poorDeveloper:Developer<{brand:string,model:string,price:number}>={
+// const poorDeveloper:Developer<CasioWatch,null>={
+const poorDeveloper:Developer<CasioWatch>={
+    name:"shahid",
+    computer:{
+        brand:"hp",
+        model:"234WA",
+        releaseYear:2015
+    },
+    // smartWatch:"casio" //akane confirm holam type hobe string so T er jaigai string bosie dilam
+    smartWatch:{
+        brand:"casio",
+        model:"C-430",
+        price:7766
+    }
+
+}
 
 
 
